f479f04e6d6574e10f87eac615147ae4eaa001f2
my namemahdikhakshoor <m.khakshoor79@gmail.com>
2021-08-23 17:25:56 +0430

    change class names

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector(".header__hamburger");
+const navMenu = document.querySelector(".header__nav-menu");
 
 hamburger.addEventListener("click", mobileMenu);
 
@@ -7,7 +7,7 @@ function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 }
-const navLink = document.querySelectorAll(".nav-link");
+const navLink = document.querySelectorAll(".header__nav-link");
 
 navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
